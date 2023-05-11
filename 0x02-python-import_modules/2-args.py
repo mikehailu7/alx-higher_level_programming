b3feb51305385e36ff1e3d4eea259b343b656d4e
mikehailu7 <mikehailu7@gmail.com>
2023-05-11 04:50:18 -0700

    2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,10 +3,10 @@
 import sys
 count = len(sys.argv) - 1
 if count == 1:
-    print("1 is the arguments.")
+    print("1 argument.")
 elif count == 0:
-    print("0 is the argument:")
+    print("0 argument:")
 else:
-    print("{} the argument are:".format(count))
+    print("{} arguments:":".format(count))
 for z in range(count):
      print("{}: {}".format(z + 1, sys.argv[z + 1]))
