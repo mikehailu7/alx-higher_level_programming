9657d5cc0a893df23537b4046dd2b035afe8e3f7
mikehailu7 <mikehailu7@gmail.com>
2023-05-11 05:39:33 -0700

    3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,7 +2,7 @@
 # mikiasHailu
 if __name__ == "__main__":
     import sys
-    total = 0
+    sumt = 0
     for i in range(len(sys.argv) - 1):
         total += int(sys.argv[i + 1])
-    print("{}".format(total))
+    print("{}".format(sumt))
