db13401a3a0f496e2e916c07b7b8aa19ea8d46a8
mikehailu7 <mikehailu7@gmail.com>
2023-05-11 04:52:23 -0700

    2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-# mikiasHailu
+
 import sys
+
 count = len(sys.argv) - 1
 if count == 1:
-    print("1 argument.")
+    print("1 arguments.")
 elif count == 0:
     print("0 argument:")
 else:
-    print("{} arguments:":".format(count))
+    print("{} arguments:".format(count))
 for z in range(count):
      print("{}: {}".format(z + 1, sys.argv[z + 1]))
