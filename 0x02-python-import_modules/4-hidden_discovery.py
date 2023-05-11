2841b2f0c2d0cd8b386749c8d38221e96b9f7a2d
mikehailu7 <mikehailu7@gmail.com>
2023-05-11 05:50:56 -0700

    4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
-if __name__ == "__main__":
+if __names__ == "__main__":
     import hidden_4
 
     people = dir(hidden_4)
-    for name in people:
-        if name[:2] != "__":
-            print(name)
+    for names in people:
+        if names[:2] != "__":
+            print(names)
