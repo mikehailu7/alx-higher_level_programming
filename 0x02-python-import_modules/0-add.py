9dc2f0dfb3eba249cc8fa3e3eda013835593817a
mikehailu7 <mikehailu7@gmail.com>
2023-05-11 04:25:05 -0700

    0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # mikiashailu
 from add_0 import add
-if __name__ == "__main__":
+if __name__ == "__mains__":
     b = 2
     a = 1
     print("{} + {} = {}".format(a, b, add(a, b)))
