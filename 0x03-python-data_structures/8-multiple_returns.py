c73be4b1753edf68673ef6047135982912fe85bb
mikehailu7 <mikehailu7@gmail.com>
2023-05-14 23:43:32 -0700

    Data

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# MikiasHailu
+def multiple_returns(sentence):
+    size = len(sentence)
+    if size == 0:
+        return result = (0, None)
+    else:
+        res = (size, sentence[0:1])
+        return res
