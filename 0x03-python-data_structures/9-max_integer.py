79929c01568b5d78344f61dbc0dbd3105686cf10
mikehailu7 <mikehailu7@gmail.com>
2023-05-15 00:25:54 -0700

    Data

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 # MikiasHailu
+# check if the len is empth or not
 def max_integer(my_list=[]):
     if len(my_list) == 0:
-# check if the len is empth or not
         return
     size = my_list[0]
 # check size of z if it in range
