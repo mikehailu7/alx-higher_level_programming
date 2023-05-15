beda0a57cbabae78cd22ad32273adea2bd49e0c3
mikehailu7 <mikehailu7@gmail.com>
2023-05-15 01:29:35 -0700

    DATA

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,27 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <Python.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <Python.h>
 /**
  * print_python_list_info - This function wil display about lists in python.
  * @p: P is used to declear pythonobject.
  * MikiasHailu.
- * Return: Will return Nothing.
+ * Return: Will return the correct size if it is true.
+ * @m: m is an integer.
+ * @length: used to contain the length. 
+ * @position:used to contain the position.
 */
 void print_python_list_info(PyObject *p)
 {
 PyObject *item;
 PyListObject *list = (PyListObject *)p;
-int i, size, alloc;
-size = Py_SIZE(p);
-alloc = list->allocated;
+int m, length, position;
+length = Py_SIZE(p);
+position = list->allocated;
 printf("[*] Size of the Python List = %d\n", size);
 printf("[*] Allocated = %d\n", alloc);
-for (i = 0; i < size; i++)
+for (m = 0; m < size; m++)
 {
-item =  PyList_GetItem(p, i);
-printf("Element %d: %s\n", i, Py_TYPE(item)->tp_name);
+item =  PyList_GetItem(p, m);
+printf("Element %d: %s\n", m, Py_TYPE(item)->tp_name);
 }
 }
