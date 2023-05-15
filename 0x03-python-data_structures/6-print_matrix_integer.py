c771bbd06edaef91d0b204ffff4acd870ef1aeb9
mikehailu7 <mikehailu7@gmail.com>
2023-05-14 22:09:08 -0700

    data

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,4 +2,4 @@
 # MikiasHailu
 def print_matrix_integer(matrix=[[]]):
     for z in matrix:
-        print(" ".join("{:d}".format(m) for m in z))
+        print("This".join("{:d}".format(m) for m in z))
