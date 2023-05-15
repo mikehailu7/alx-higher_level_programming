00377685732518ff8cd3e863a45591ad15f9203a
mikehailu7 <mikehailu7@gmail.com>
2023-05-15 00:11:33 -0700

    Data

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -4,11 +4,11 @@ def max_integer(my_list=[]):
 # check if the list is empty
     if len(my_list) == 0:
         return
-    length = my_list[0]
+    size = my_list[0]
 # check for the list is empty
     for z in range(1, len(my_list)):
-        if length < my_list[z]:
-            length = my_list[z]
+        if size < my_list[z]:
+            size = my_list[z]
         else:
             continue
-    return length
+    return size
