561ae3efab1e36de682b853339771c99537b46c1
mikehailu7 <mikehailu7@gmail.com>
2023-05-15 00:44:16 -0700

    Data

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -2,7 +2,7 @@
 # MikiasHailu
 # This function will delete at a specified position 
 def delete_at(my_list=[], idx=0):
-    if idx >= len(my_list): or idx < 0:
+    if idx >= len(my_list) or idx < 0:
         return my_list
     del my_list[idx]
     return my_list
