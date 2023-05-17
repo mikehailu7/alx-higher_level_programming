10c92b380c3976b3e2df4e688fea79d5fcf5cc3f
mikehailu7 <mikehailu7@gmail.com>
2023-05-17 03:53:06 -0700

    Data2

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -3,10 +3,10 @@
 # Waverage
 def weight_average(my_list=[]):
     if my_list and len(my_list):
-        scr = 0
-        wei = 0
+        num = 0
+        dem = 0
         for chc in my_list:
-            wei = wei + chc[1]
-            scr = scr + (chc[0] * chc[1])
+            dem = dem + chc[1]
+            num = num + (chc[0] * chc[1])
         return (num / dem)
     return 0
