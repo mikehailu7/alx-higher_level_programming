2932b1bf4e66f8d6d3b8f49f4862e5c50a2938c4
mikehailu7 <mikehailu7@gmail.com>
2023-05-30 05:22:18 -0700

    class

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -2,8 +2,7 @@
 # Author: Mikiashailu
 # Myfirstsquare
 
-Square = __import__('0-square').Square
-"""This is a class of square."""
+"""This is a class of square"""
 class Square:
-    """This is an empty square."""
+    """This is an empty square"""
     pass
