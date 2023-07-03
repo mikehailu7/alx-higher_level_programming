8fbbcab81e1cb4bab9d209af908d303fc50f90da
mikehailu7 <mikehailu7@gmail.com>
2023-07-03 11:46:02 -0700

    rectangel

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
-# author: mikiashailu
-# fun: rectanglethree
-"""
-This function will define a class Rectangle
-"""
+# Author: mikiasHailu
+# Fun: rectangleThree
+
+""" This function will define a class Rectanglev """
+
+
 class Rectangle:
-    """This function is about a rectangle"""
+    """ This function is about a rectangle"""
     def __init__(self, width=0, height=0):
         """This is the declaration of the rectangle"""
         self.width = width
         self.height = height
-     @property
+
+    @property
     def height(self):
         """This function shows the height"""
         return self.__height
@@ -23,6 +25,7 @@ class Rectangle:
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
+
     @property
     def width(self):
         """This function shows the width"""
@@ -36,13 +39,14 @@ class Rectangle:
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
+
     def __str__(self):
         """returns printable string representation of the rectangle"""
         string = ""
         if self.__width != 0 and self.__height != 0:
-            string += "\n".join("#" * self.__width
-                                for j in range(self.__height))
-        return string
+            string = String + "\n".join("#" * self.__width
+                    for j in range(self.__height))
+            return string
     def area(self):
         """This fucntion will return the area of the rectangle"""
         return self.__width * self.__height
@@ -52,13 +56,3 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return 0
         return (self.__width * 2) + (self.__height * 2)
-
-
-
-    def __str__(self):
-        """returns printable string representation of the rectangle"""
-        string = ""
-        if self.__width != 0 and self.__height != 0:
-            string += "\n".join("#" * self.__width
-                                for j in range(self.__height))
-        return string
