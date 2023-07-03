51d8ab4d6c14b83f8a00d274cb81be84f5bc798e
mikehailu7 <mikehailu7@gmail.com>
2023-07-03 12:25:29 -0700

    rectangel

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -54,6 +54,6 @@ class Rectangle:
         """ This function will returns printable string of the rectangle """
         string = ""
         if self.__width != 0 and self.__height != 0:
-            string = string + "\n".join("#" * self.__width
+            string += "\n".join("#" * self.__width
                     for m in range(self.__height))
             return string
