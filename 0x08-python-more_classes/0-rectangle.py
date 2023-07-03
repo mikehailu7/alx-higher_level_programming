1a470db6646f30c62426d268c8c0b430fee8867c
mikehailu7 <mikehailu7@gmail.com>
2023-07-03 10:32:49 -0700

    rectangle

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-"""
-This will show an empty class Rectangle
-"""
+# Author: mikiasHailu
+# Fun: rectangleone
+
+""" This will show an empty class Rectangle """
 class Rectangle:
-    """This will representation of an empty rectangle"""
+    """ This is representation of an empty rectangle """
     pass
