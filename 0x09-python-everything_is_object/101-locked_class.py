ad9a4e6dc497612e43ec10afdd30489cb51380ae
mikehailu7 <mikehailu7@gmail.com>
2023-07-04 04:57:45 -0700

    everything

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# Author: MikiasHailu
+
+""" This class will describe a lockedclass."""
+
+class LockedClass:
+    """ This is the class declaration  """
+
+    __slots__ = ["first_name"]
