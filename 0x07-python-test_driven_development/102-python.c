e5154a96a79651ff94c468d38b4482e7252f460c
mikehailu7 <mikehailu7@gmail.com>
2023-07-04 13:46:55 -0700

    tests

---
+++ 0x07-python-test_driven_development/102-python.c
@@ -0,0 +1,30 @@
+#include "Python.h"
+
+/**
+ * print_python_string: This function will Prints information about Python strings.
+ * @p: A PyObject string object.
+ * Author: MIkiasHailu
+ * project: pythonstrings
+ */
+void print_python_string(PyObject *p)
+{
+	long int leng;
+
+	fflush(stdout);
+
+	printf("[.] string object info\n");
+	if (strcmp(p->ob_type->tp_name, "str") != 0)
+	{
+		printf("  [ERROR] Invalid String Object\n");
+		return;
+	}
+
+	leng = ((PyASCIIObject *)(p))->leng;
+
+	if (PyUnicode_IS_COMPACT_ASCII(p))
+		printf("  type: compact ascii\n");
+	else
+		printf("  type: compact unicode object\n");
+	printf("  length: %ld\n", leng);
+	printf("  value: %ls\n", PyUnicode_AsWideCharString(p, &leng));
+}
