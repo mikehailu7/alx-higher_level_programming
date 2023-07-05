2ac4dbef3982fd0f2fad578945eedc818f79153c
mikehailu7 <mikehailu7@gmail.com>
2023-07-05 00:10:41 -0700

    tests

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -9,7 +9,7 @@
 
 void print_python_string(PyObject *p)
 {
-	long int leng;
+	long int length;
 
 	fflush(stdout);
 
@@ -20,12 +20,12 @@ void print_python_string(PyObject *p)
 		return;
 	}
 
-	leng = ((PyASCIIObject *)(p))->leng;
+	length = ((PyASCIIObject *)(p))->length;
 
 	if (PyUnicode_IS_COMPACT_ASCII(p))
 		printf("  type: compact ascii\n");
 	else
 		printf("  type: compact unicode object\n");
-	printf("  leng: %ld\n", leng);
-	printf("  value: %ls\n", PyUnicode_AsWideCharString(p, &leng));
+	printf("  length: %ld\n", length);
+	printf("  value: %ls\n", PyUnicode_AsWideCharString(p, &length));
 }
