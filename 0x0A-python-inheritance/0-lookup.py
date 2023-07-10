cf5ff69967feefad434f085872743f01ee28a9ef
mikehailu7 <mikehailu7@gmail.com>
2023-07-10 04:15:11 -0700

    inheritance

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -2,9 +2,8 @@
 #Author: mikiasHailu
 #project: inheritance
 
-""" This function is called the lookup """
-
+"""This function is called the lookup"""
 
 def lookup(obj):
-    """ this function have obj as a parameter and return that obj """
+    """this function have obj as a parameter and return that obj"""
     return dir(obj)
