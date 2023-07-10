2120c2e29eed4807374db151d5ec63fc2e839fec
mikehailu7 <mikehailu7@gmail.com>
2023-07-10 08:37:56 -0700

    inheritance

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-#Author: MIkiasHailu
-#Project: Square
+# Author: MIkiasHailu
+# Project: Square
 """ This is the square class """
 
 Rectangle = __import__('9-rectangle').Rectangle
 
+
 class Square(Rectangle):
     """ This is the declaration of a square class """
+
     def __init__(self, size):
         """ This fucntion is the initialization of the abouve class """
         self.integer_validator("size", size)
