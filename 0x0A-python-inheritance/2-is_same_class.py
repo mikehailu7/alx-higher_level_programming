af72a39ae99e217fad76f4f100fb0b83c885c49d
mikehailu7 <mikehailu7@gmail.com>
2023-07-10 04:49:37 -0700

    inheritance

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-# Author: mikiasHailu
-# Project: Exact same object
+# Author: MikiasHailu
+# Project: ExactSameObject
 """ This is the is same class function """
 
+
+
 def is_same_class(obj, a_class):
-    """ This function has to partameters and return the obj is the exact class if true"""
+    """This function has to partameters and return the obj is the exact class if true"""
     return (type(obj) == a_class)
