7e8e3158f957aae43f8cea35d5e8f8d6d76781e3
mikehailu7 <mikehailu7@gmail.com>
2023-07-10 09:34:54 -0700

    inheritance

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,37 +1,36 @@
 #!/usr/bin/python3
 # Author: MikiasHailu
-# Project: Full rectangle
-""" This module contains the Rectangle and BaseGeometry class """
+# Project: FullRectangle
+""" This module contains the Rectangle and BaseGeometery """
+
+
+class BaseGeometry:
+    """A class with public instance methods area and integer_validator"""
+    def area(self):
+        """raises an exception when called"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """validates that value is an integer greater than 0"""
+        if type(value) is not int:
+            raise TypeError("{:s} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{:s} must be greater than 0".format(name))
+
 
 class Rectangle(BaseGeometry):
-    """ This is a rectangle class """
-
+    """A representation of a rectangle"""
     def __init__(self, width, height):
-        """ This function is the initialization of the rectangle """
+        """instantiation of the rectangle"""
         self.integer_validator("width", width)
         self.__width = width
         self.integer_validator("height", height)
         self.__height = height
 
-    def __str__(self):
-        """ This function is informal string representation of a rectangle"""
-        return "[Rectangle] {:d}/{:d}".format(self.__width, self.__height)
-
     def area(self):
-        """ This function will calculate the area of the rectangle """
+        """returns the area of the rectangle"""
         return self.__width * self.__height
 
-
-class BaseGeometry:
-    """ THis class is base geomery class """
-
-    def area(self):
-        """ This is an area function with exception. """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """ This function will check that value is an integer greater than 0 """
-        if type(value) is not int:
-            raise TypeError("{:s} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{:s} must be greater than 0".format(name))
+    def __str__(self):
+        """informal string representation of the rectangle"""
+        return "[Rectangle] {:d}/{:d}".format(self.__width, self.__height)
