0b727ea525bfc823288d5155ce0cc8b2402eb6f1
mikehailu7 <mikehailu7@gmail.com>
2023-07-10 04:54:22 -0700

    inheritance

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 # Author: MikiasHailu
-# Project: impove geometry
+# Project: ImpoveGeometry
 """ This class is called the BaseGeometry """
 
 
 class BaseGeometry:
-
+    """ This class is a baseGeometery class """
     def area(self):
         """ This is the area function with an expec """
         raise Exception("area() is not implemented")
