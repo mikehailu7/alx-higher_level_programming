0645351dd0362bc1384fbe39d3ed6b644584843f
mikehailu7 <mikehailu7@gmail.com>
2023-07-10 04:51:33 -0700

    inheritance

--- 0x0A-python-inheritance/5-base_geometry.py
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-#Author: mikiashailu
-#Project: Geometery module
+# Author: mikiashailu
+# Project: Geometery module
 """ This fucntion is called the base geometery """
+
 
 class BaseGeometry:
     """ This class contains nothingAn """
