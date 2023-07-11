37362781c0ce7cf8bf238de11d6ba77b3ce4da76
mikehailu7 <mikehailu7@gmail.com>
2023-07-11 02:45:06 -0700

    input&output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-"""read_file module.
-
-Contains a function that reads a text file.
-"""
-
+# Author: MikiasHailu
+# Project: Readfile
+""" This function is to read a file """
 
 def read_file(filename=""):
-    """Reads a text file (UTF8) and prints it to stdout."""
+    """ This function will Reads a text file. """
     with open(filename, 'r') as f:
         print(f.read(), end='')
