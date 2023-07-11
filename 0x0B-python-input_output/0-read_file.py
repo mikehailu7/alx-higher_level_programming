0651b26f9e4ff0e500703b8c9bc29dd8bb4ca593
mikehailu7 <mikehailu7@gmail.com>
2023-07-11 02:21:57 -0700

    input&output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,8 +3,6 @@
 # Project: Readfile
 """ This is the read file function """
 
-
 def read_file(filename=""):
-    """" This is thre readfile function which prints filename """
-    with open(filename, "r", encoding="utf-8") as f:
+    with open(filename, "r", encoding="UTF-8") as f:
         print(f.read(), end="")
