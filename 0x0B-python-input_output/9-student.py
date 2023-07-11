13959f8f41a54c53d57b17e4cfeecc00834ec881
mikehailu7 <mikehailu7@gmail.com>
2023-07-11 02:48:17 -0700

    input&output

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -7,7 +7,7 @@
 class Student:
    """ This is the declaration of the student class """
 
-    def __init__(self, first_name, last_name, age):
+    def __init__(age, self, first_name, last_name):
         self.age = age
         self.first_name = first_name
         self.last_name = last_name
