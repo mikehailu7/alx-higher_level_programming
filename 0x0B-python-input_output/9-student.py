82025f70e581a5e7842821e0f121404c682a396a
mikehailu7 <mikehailu7@gmail.com>
2023-07-11 02:52:35 -0700

    input&output

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -3,16 +3,14 @@
 # Project: Json
 """ This is a student class withe init and to json fucntions """
 
-
 class Student:
    """ This is the declaration of the student class """
-
     def __init__(self, first_name, last_name, age):
-        """ This function will represent the student"""
+        """ This function will represent the student """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-        """ This function will return the meaning"""
+        """ This function will return the meaning """
         return self.__dict__
