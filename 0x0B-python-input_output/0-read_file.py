efb56d38f08e7411dd8ae8b750fb97c3954d1489
mikehailu7 <mikehailu7@gmail.com>
2023-07-11 22:42:01 -0700

    input&output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,6 +4,6 @@
 """ This function is to read a file """
 
 def read_file(filename=""):
-    """ This function will Reads a text file """
-    with open(filename, 'r') as f:
-        print(f.read(), end='')
+    """ This function will read a file """
+    with open(filename, "r", encoding="utf-8") as f:
+        print(f.read(), end="")
