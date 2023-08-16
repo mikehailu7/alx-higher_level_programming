04996ff89cf818ae3761d5768ae7a682c815c5be
mikehailu7 <mikehailu7@gmail.com>
2023-08-16 02:28:25 -0700

    sql more

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,6 @@
 -- Author: Mikias Hailu
 -- This query script that lists all cities contained in the database
-SELECT m.`id`, m.`name`, m.`name`
+SELECT m.`id`, m.`name`, n.`name`
   FROM `cities` AS m
        INNER JOIN `states` AS n
        ON m.`state_id` = n.`id`
