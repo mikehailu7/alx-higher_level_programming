c400d6a43252b6eff313d20886ce26f904d43fcd
mikehailu7 <mikehailu7@gmail.com>
2023-08-16 05:19:50 -0700

    sql more

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,7 @@
 -- Author: Mikias Hailu
--- This query script to run msql commands
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.*TO 'user_0d_1'@'localhost'IDENTIFIED BY 'user_0d_1_pwd';
+-- This query will create user od 1 with all previlages
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
+IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.*
+TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
