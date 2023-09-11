8c403842cec02f32a6debc8a399d5f749440f68d
mikehailu7 <mikehailu7@gmail.com>
2023-09-11 03:55:30 -0700

    javascript

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 // Author: Mikias Hailu
-function factorial (n) {
+function factorial (m) {
   return m === 0 || isNaN(m) ? 1 : m * factorial(m - 1);
 }
 console.log(factorial(Number(process.argv[2])));
