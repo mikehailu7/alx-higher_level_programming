af76a6cf720c12f3e6945c43736a72fef5e88ec5
mikehailu7 <mikehailu7@gmail.com>
2023-10-24 05:23:52 -0700

    web scraping

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,6 +1,8 @@
 #!/usr/bin/node
-# Author: MikiasHailu
 const request = require('request');
-request.get(process.argv[2]).on('response', function (response) {
-	console.log(`code: ${response.statusCode}`);
+request(process.argv[2], function (error, response) {
+	if (error) {
+		console.error(error);
+	}
+	console.log('code:', response && response.statusCode);
 });
