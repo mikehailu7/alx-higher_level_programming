e1c77e34c3cb3044bedb51bdceba26c4abe1ba70
mikehailu7 <mikehailu7@gmail.com>
2023-10-24 04:53:18 -0700

    web scraping

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,10 +1,5 @@
 #!/usr/bin/node
-# Author: MikiasHailu
 const fs = require('fs');
-fs.readFile(process.argv[2], 'utf-8', function (err, result) {
-	if (err) {
-		console.log(err);
-	} else {
-		console.log(result);
-	}
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
+	console.log(error || content);
 });
